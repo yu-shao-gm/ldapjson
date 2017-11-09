3b21616e62e476c1951d83d4a2f1f408f11f3cee
Yu Shao <yu.shao.gm@gmail.com>
2017-11-09 11:37:21 -0500

    Update ldapjson.java

--- src/main/java/org/jboss/as/quickstarts/ldapjson/ldapjson.java
+++ src/main/java/org/jboss/as/quickstarts/ldapjson/ldapjson.java
@@ -81,7 +81,7 @@ public class ldapjson {
             LdapConnection connection = new LdapNetworkConnection( "ldap.corp.redhat.com", 389 );
 
             //EntryCursor cursor = connection.search( "ou=users,dc=redhat,dc=com", "(rhathiredate>=20171106000000Z)", SearchScope.ONELEVEL );
-            EntryCursor cursor = connection.search( "ou=users,dc=redhat,dc=com", "(rhathiredate>="+joindate+")", SearchScope.ONELEVEL, "'*' '+'" );
+            EntryCursor cursor = connection.search( "ou=users,dc=redhat,dc=com", "(rhathiredate>="+joindate+")", SearchScope.ONELEVEL, "* +" );
 
             JsonFactory factory = new JsonFactory();
 
